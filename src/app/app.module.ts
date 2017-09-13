12d220999f9d0fd1ff91d6fbac7f9a847d6ec61d
Bohdan-Ivan Maliuha <bogdan.maliuga@gmail.com>
2017-09-13 16:56:08 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './components/hero/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
+
   ],
   imports: [
     BrowserModule
